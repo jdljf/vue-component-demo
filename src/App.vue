f5396ca391cf4a92c9e32f5f04b81a148449faff
jdljf <1243270526@qq.com>
2019-06-01 16:33:02 +0800

    Vue 封装一个组件

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <div>{{msg}}</div>
+    <vueCountup :end='2017' :start='100' :options='options'></vueCountup>
+  </div>
+</template>
+
+<script>
+import vueCountup from './index'
+
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      options:{
+        duration: 3
+      }
+    };
+  },
+  mounted() {
+  },
+  methods:{
+  },
+  components:{
+    vueCountup
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <span ref="countup" id="countup"></span>
+</template>
+
+<script>
+import { CountUp } from "countup.js";
+
+export default {
+  name: "app",
+  props: {
+    start: {
+      type: Number,
+      default: 0
+    },
+    end: {
+      type: Number,
+      default: 2017
+    },
+    decimal: {
+      type: Number,
+      default: 0
+    },
+    duration: {
+      type: Number,
+      default: 2.5
+    },
+    options: {
+      type: Object
+    },
+    autoplay: {
+      type: Boolean,
+      required: false,
+      default: true
+    }
+  },
+  data() {
+    return {
+      numAnim: null,
+      // options:{
+
+      // }
+    };
+  },
+  mounted() {
+    this.initCountUp();
+  },
+  methods: {
+    initCountUp() {
+      this.options.startVal = this.start
+      this.numAnim = new CountUp(this.$refs.countup, this.end, this.options);
+      if (!this.numAnim.error && this.autoplay) {
+        this.numAnim.start();
+      } else {
+        console.log(this.numAnim.error);
+      }
+    }
+  }
+};
+</script>
+
